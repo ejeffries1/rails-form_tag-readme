d57753bd2474375177bc5ece8e16476d583240be
evan <ejeffries14@gmail.com>
2022-01-03 01:42:42 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index]
+
+  get '/new', to: 'posts#new'
+
+  resources :posts, only: [:index, :new, :create]
 end
